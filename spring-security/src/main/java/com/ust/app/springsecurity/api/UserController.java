bf9d5a27a3bb65ae097c93c4c5e0c5a98ce9efe4
KodiBharadwaj <bharadwaj21946@gmail.com>
2024-11-28 12:09:31 +0530

    updated list

--- spring-security/src/main/java/com/ust/app/springsecurity/api/UserController.java
+++ spring-security/src/main/java/com/ust/app/springsecurity/api/UserController.java
@@ -1,5 +1,7 @@
 package com.ust.app.springsecurity.api;
 
+import com.ust.app.springsecurity.dto.ApiResponseMessage;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,11 @@ import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin
 public class UserController {
 
     @GetMapping
-    public String sayHello(Principal principal){
-        return "Hello "+principal.getName();
+    public ApiResponseMessage sayHello(){
+        return new ApiResponseMessage("Hello User");
     }
 }
